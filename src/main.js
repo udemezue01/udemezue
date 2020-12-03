b631c1bb7246978d3c2464062260543e44d8d45c
Udemezue John <johnzeus14@gmail.com>
2020-12-04 00:20:17 +0100

    added Vue appollo client

--- src/main.js
+++ src/main.js
@@ -6,10 +6,44 @@ import store from './store'
 
 import '@/assets/css/tailwinds.css'
 
+import VueMeta from 'vue-meta'
+
+import VueApollo from 'vue-apollo'
+
+
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3020/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+Vue.use(VueMeta)
+
+Vue.use(VueApollo)
+
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
