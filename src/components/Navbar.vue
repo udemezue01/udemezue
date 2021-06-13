95d41d2c1abee43d36c81cd1229a996fb7c00686
Nwabufor John <johnzeus14@gmail.com>
2021-06-13 21:32:12 +0100

    updated navar components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,9 +37,9 @@
       </div>
 
       <!-- The navbar toggle section -->
-      <div class="lg:hidden">
+      <div class="lg:hidden items-center">
 
-        
+        <svg class="w-6 h-6 text-gray-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
         
       </div>
 
