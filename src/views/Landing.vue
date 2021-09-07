383c4c2b3288b831f370c7ef1fc93918f5ba106f
Nwabufor John <johnzeus14@gmail.com>
2021-09-07 19:32:21 +0100

    updated landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -7,12 +7,12 @@
 
     <section class="flex mt-32 px-6 lg:px-32 space-x-64 items-center">
 
-      <div class="w-full lg:w-1/2 space-y-10">
+      <div class="w-full space-y-10 lg:w-1/2 ">
 
-        <h1 class="font-body text-gray-800 text-justify tracking-wider font-extrabold text-4xl"> Hi, I'm Udemezue John</h1>
+        <h1 class="font-body text-gray-800 tracking-wider font-extrabold text-4xl text-center lg:text-left"> Hi, I'm Udemezue John</h1>
         
         
-        <p class="text-xl leading-relaxed font-body tracking-widest text-gray-700 font-medium"> 
+        <p class="text-xl leading-relaxed font-body tracking-widest text-gray-700 font-medium text-center lg:text-left"> 
             An enthusiastic web developer  with a rich experience in Web development, & Digital Marketer
           
            </p>
@@ -20,7 +20,7 @@
 
             <!-- The social Media section -->
 
-          <div class="flex space-x-8 items-center">
+          <div class="flex justify-center lg:justify-start space-x-8 items-center mx-auto">
         <a href=""> <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook h-8 w-8 rounded-full hover:bg-red-300"  viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
@@ -48,7 +48,7 @@
     <!-- Call to action section starts -->
 
 
-        <div class="flex items-center space-x-6 mt-12">
+        <div class="flex justify-center lg:justify-start items-center space-x-6 mt-12">
 
           <router-link to="/portfolio"class="bg-gray-800 text-white py-3 px-4 font-body font-medium inline-flex space-x-2 rounded-md">
            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
