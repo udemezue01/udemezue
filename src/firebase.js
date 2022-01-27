5e5c1ed3545ca72439d45cddf0fa7c4408f9f778
Nwabufor John <johnzeus14@gmail.com>
2022-01-27 15:22:50 +0100

    updated browser support

--- src/firebase.js
+++ src/firebase.js
@@ -17,5 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
