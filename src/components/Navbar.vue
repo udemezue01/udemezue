74b17bd2639ac0da229515fbb73d7bbe395c94bc
Nwabufor John <johnzeus14@gmail.com>
2022-03-27 14:42:06 +0100

    Updated navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,34 +3,42 @@
 
   <!--   The navigational bar section -->
 
-    <nav class="bg-gray-50 flex justify-between py-4 px-6 lg:px-32 items-center shadow-sm">
+    <nav class="bg-white flex justify-between py-4 px-6 lg:px-32 items-center shadow-sm">
       
       <div class="text-white">
 
         <router-link to ="/" class="text-3xl font-body font-black tracking-wider bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-blue-600">
 
-     U
+        <img src="/img/helmet.svg" alt=""  class="h-10 w-10">
       </router-link>
       </div>
 
-      <div class="flex items-center space-x-4 hidden lg:block">
+      <div class="flex items-center space-x-4 hidden lg:block text-lg">
 
-        <a href="" class="capitalize font-body text-md tracking-widest px-3 py-3 hover:bg-green-200 rounded-md "> projects</a>
+        <a href="" class="capitalize font-body text-md tracking-widest px-3 py-3 hover:bg-gray-50 rounded-md "> Home</a>
 
-        <a href="" class="capitalize font-body text-md tracking-widest px-3 py-3 hover:bg-green-200 rounded-md"> resume</a>
+        <a href="" class="capitalize font-body text-md tracking-widest px-3 py-3 hover:bg-gray-50 rounded-md"> About</a>
 
-        <a href="" class="capitalize font-body text-md tracking-widest px-3 py-3 hover:bg-green-200 rounded-md"> talks</a>
+        <a href="" class="capitalize font-body text-md tracking-widest px-3 py-3 hover:bg-gray-50 rounded-md"> Portfolio</a>
 
-        <a href="" class="capitalize font-body text-md tracking-widest px-3 py-3 hover:bg-green-200 rounded-md"> interests</a>
+        <a href="" class="capitalize font-body text-md tracking-widest px-3 py-3 hover:bg-gray-50 rounded-md"> Resume</a>
         
 
       </div>
 
       
 
-      <div class="flex items-center hidden lg:block">
+      <div class="flex items-center space-x-3 font-body tracking-wider text-lg">
 
-        <a href="https://udemezue.hashnode.dev" class="bg-gray-800 text-white px-4 py-3 font-body text-sm tracking-widest rounded-md"> Blog</a>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail-forward h-8 w-8" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M12 18h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5" />
+          <path d="M3 6l9 6l9 -6" />
+          <path d="M15 18h6" />
+          <path d="M18 15l3 3l-3 3" />
+        </svg>
+
+          <span> hi@Tchelete.com</span>
 
       
       </div>
